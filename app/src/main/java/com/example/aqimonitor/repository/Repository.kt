670ae71f27189aa6a479ffb40f13a92d9ff4d74c
jb-github123 <84889586+jb-github123@users.noreply.github.com>
2021-06-01 12:35:19 +0530

    test implementation of web socket code added

--- app/src/main/java/com/example/aqimonitor/repository/Repository.kt
+++ app/src/main/java/com/example/aqimonitor/repository/Repository.kt
@@ -1,12 +1,21 @@
 package com.example.aqimonitor.repository
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
+import com.example.aqimonitor.config.DEBUG_ON
 import com.example.aqimonitor.database.AppDatabase
 import com.example.aqimonitor.database.dao.CityAQIDao
 import com.example.aqimonitor.database.model.CityAQIData
+import com.example.aqimonitor.network.NetworkManager
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
 
 class Repository(private val application: Application) {
+
+    private val NETWORK_RESPONSE_TAG = "NETWORK_RESPONSE_TAG"
 
     private var cityAQIDao: CityAQIDao
     private val newCityAQIDataList = ArrayList<CityAQIData>()
@@ -31,16 +40,52 @@ class Repository(private val application: Application) {
      * get data from server via api call
      */
     private fun getAQIDataFromServer() {
+
+        val listener: WebSocketListener = object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                super.onOpen(webSocket, response)
+                Log.e(NETWORK_RESPONSE_TAG, "WebSocket opened - $response")
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                super.onMessage(webSocket, text)
+                Log.e(NETWORK_RESPONSE_TAG, "WebSocket response - $text")
+
+                CacheManager(application).setLastServerCallTime()
+
+                // todo loop
+                // newCityAQIDataList.add()
+                // loop
+
+                // todo save data in db
+                // saveAQIDataInDb()
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                if (DEBUG_ON) {
+                    if (DEBUG_ON) Log.e(NETWORK_RESPONSE_TAG, "WebSocket Failure - $response")
+                    t.printStackTrace()
+                }
+            }
+
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosing(webSocket, code, reason)
+                if (DEBUG_ON) Log.e(NETWORK_RESPONSE_TAG, "WebSocket Closing - $code - $reason")
+            }
+
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosed(webSocket, code, reason)
+                if (DEBUG_ON) Log.e(NETWORK_RESPONSE_TAG, "WebSocket Closed - $code - $reason")
+            }
+
+        }
+
+        val request: Request = Request.Builder().url(NetworkManager.BASE_URL).build()
+
         // todo create a socket connection and get data from server, close connection, store data in db
-        /*on success {
-            CacheManager(application).setLastServerCallTime()
-
-            // loop
-            // newCityAQIDataList.add()
-            // loop
-
-            saveAQIDataInDb()
-        }*/
+        val webSocket = NetworkManager.getInstance().newWebSocket(request, listener)
+        NetworkManager.getInstance().dispatcher.executorService.shutdown()
     }
 
     /**
