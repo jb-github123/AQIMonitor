a30a7870299c9980197f3d26cc8307bf878db93d
jb-github123 <84889586+jb-github123@users.noreply.github.com>
2021-06-01 14:12:46 +0530

    7.4.1.  Defined Status Codes
    Endpoints MAY use the following pre-defined status codes when sending
    a Close frame.
    
    1000
    
    1000 indicates a normal closure, meaning that the purpose for
    which the connection was established has been fulfilled.

--- app/src/main/java/com/example/aqimonitor/network/NetworkManager.kt
+++ app/src/main/java/com/example/aqimonitor/network/NetworkManager.kt
@@ -7,6 +7,7 @@ class NetworkManager {
     companion object {
 
         val BASE_URL = "ws://city-ws.herokuapp.com/"
+        val SOCKET_NORMAL_CLOSE = 1000
 
         @Volatile
         private var networkClient: OkHttpClient? = null
