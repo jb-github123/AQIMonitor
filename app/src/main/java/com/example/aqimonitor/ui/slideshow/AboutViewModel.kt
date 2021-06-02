0ff735949e1613bd1d83c69e7c12e015be12f67a
jb-github123 <84889586+jb-github123@users.noreply.github.com>
2021-06-02 13:47:20 +0530

    about screen content added

--- app/src/main/java/com/example/aqimonitor/ui/slideshow/AboutViewModel.kt
+++ app/src/main/java/com/example/aqimonitor/ui/slideshow/AboutViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.aqimonitor.ui.slideshow
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.aqimonitor.R
 
-class SlideshowViewModel : ViewModel() {
+class AboutViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = application.getString(R.string.text_about)
+
     }
     val text: LiveData<String> = _text
 }
